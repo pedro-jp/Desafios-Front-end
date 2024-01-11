dc0f29409bc67af3827db3c8a472dc6079f685a4
João Pedro C. Carvalho <60660447+pedro-jp@users.noreply.github.com>
2024-01-11 00:21:30 +0000

    Feat: Estilos do input

--- blog/src/app/page.js
+++ blog/src/app/page.js
@@ -5,11 +5,12 @@ export default function Home() {
     <main className={styles.main}>
       <section className={styles.cabecalho}><h1>Code<span>lândia</span></h1>
         <div>
-        <img src="./assets/svg/Vetor.png" alt="SVG Image" width="100" height="100" />
-          <input typeof='text' placeholder='Pesquisar no blog'></input>
+        <img src="/assets/svg/Vector.svg" alt="SVG Image" width="100" height="100" />
+          <input type='text' placeholder='Pesquisar no blog'></input>
         </div>
       </section>
-     
+      <section className={styles.posts}>
+     </section>
     </main>
   )
 }
