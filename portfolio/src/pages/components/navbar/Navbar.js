0f3e113c6217893546a714d81dc7394e11d98f4e
João Pedro C. Carvalho <60660447+pedro-jp@users.noreply.github.com>
2024-01-29 16:47:54 +0000

    feat: navbar style

--- portfolio/src/pages/components/navbar/Navbar.js
+++ portfolio/src/pages/components/navbar/Navbar.js
@@ -2,9 +2,9 @@ import styles from "./Navbar.module.css"
 
 const Navbar = () => {
     return(
-        <nav className={styles.navbar}>
+        <nav id="navbar" className={styles.navbar}>
             <ul>
-                <li><a href="">Pedro<span>Dev</span></a></li>
+                <li><a href="#navbar">Pedro<span>Dev</span></a></li>
                 <li>
                     <ul>
                         <li><a href="">Sobre</a></li>
@@ -16,7 +16,10 @@ const Navbar = () => {
                 <ul>
                         <li><a href="">LinkedIn</a></li>
                         <li><a href="">Github</a></li>
-                        <li>Fale comigo</li>
+                        <li id={styles.contato}>Fale comigo 
+                            <span><a href="">Whatsapp</a></span>
+                            <span><a href="">E-mail</a></span>
+                        </li>
                     </ul>
                 </li>
             </ul>
