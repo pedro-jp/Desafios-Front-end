4a121a7bf0083150887e0af85f3501d2a824ace4
João Pedro C. Carvalho <60660447+pedro-jp@users.noreply.github.com>
2024-01-30 12:40:50 +0000

    feat: menu hamburguer

--- portfolio/src/pages/components/navbar/Navbar.js
+++ portfolio/src/pages/components/navbar/Navbar.js
@@ -3,11 +3,16 @@ import styles from "./Navbar.module.css"
 const Navbar = () => {
     return(
         <nav id="navbar" className={styles.navbar}>
+            <div id={styles.hamburguer}>
+                <span className={styles.Span} id={styles.um}></span>
+                <span className={styles.Span} id={styles.dois}></span>
+                <span className={styles.Span} id={styles.tres}></span>
+            </div>
             <ul>
                 <li><a href="#navbar">Pedro<span>Dev</span></a></li>
                 <li>
                     <ul>
-                        <li><a href="">Sobre</a></li>
+                        <li><a href="#sobre">Sobre</a></li>
                         <li><a href="">Skills</a></li>
                         <li><a href="">Projetos</a></li>
                     </ul>
