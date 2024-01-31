613ce4c06b4f3aa08e762ce1f1b215f3a13cec50
João Pedro C. Carvalho <joaopedroc035@gmail.com>
2024-01-31 10:29:57 -0300

    🦄 refactor: change layout

--- portfolio/src/pages/components/navbar/Navbar.js
+++ portfolio/src/pages/components/navbar/Navbar.js
@@ -1,35 +1,51 @@
-import styles from "./Navbar.module.css"
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-    return(
-        <nav id="navbar" className={styles.navbar}>
-            <div id={styles.hamburguer}>
-                <span className={styles.Span} id={styles.um}></span>
-                <span className={styles.Span} id={styles.dois}></span>
-                <span className={styles.Span} id={styles.tres}></span>
-            </div>
-            <ul>
-                <li><a href="#navbar">Pedro<span>Dev</span></a></li>
-                <li>
-                    <ul>
-                        <li><a href="#sobre">Sobre</a></li>
-                        <li><a href="">Skills</a></li>
-                        <li><a href="">Projetos</a></li>
-                    </ul>
-                </li>
-                <li>
-                <ul>
-                        <li><a href="">LinkedIn</a></li>
-                        <li><a href="">Github</a></li>
-                        <li id={styles.contato}>Fale comigo 
-                            <span><a href="">Whatsapp</a></span>
-                            <span><a href="">E-mail</a></span>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav id="navbar" className={styles.navbar}>
+      <div id={styles.hamburguer}>
+        <span className={styles.Span} id={styles.um}></span>
+        <span className={styles.Span} id={styles.dois}></span>
+        <span className={styles.Span} id={styles.tres}></span>
+      </div>
+      <ul>
+        <li>
+          <a href="#navbar">
+            Pedro<span>Dev</span>
+          </a>
+        </li>
+        <li>
+          <ul>
+            <li>
+              <a href="#sobre">Sobre</a>
+            </li>
+            <li>
+              <a href="">Skills</a>
+            </li>
+            <li>
+              <a href="">Projetos</a>
+            </li>
+
+            <li>
+              <a href="">LinkedIn</a>
+            </li>
+            <li>
+              <a href="">Github</a>
+            </li>
+            <li id={styles.contato}>
+              Fale comigo
+              <span>
+                <a href="">Whatsapp</a>
+              </span>
+              <span>
+                <a href="">E-mail</a>
+              </span>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
