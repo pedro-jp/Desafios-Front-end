39529f3581266eb249ddb2ccd6c0b4ab6b379f45
João Pedro C. Carvalho <60660447+pedro-jp@users.noreply.github.com>
2024-02-01 16:29:43 +0000

    feat: adicionada imagem

--- portfolio/src/pages/components/hero/Hero.js
+++ portfolio/src/pages/components/hero/Hero.js
@@ -8,7 +8,7 @@ const Hero = () => {
             <h2>Olá, eu sou</h2>
             <h1>
               &lt;Pedro <br /> Dev /&gt;
-            </h1>
+            </h1> 
             <h2 style={{ fontSize: "2rem", marginTop: "20px" }}>
               Full Stack Developer
             </h2>
@@ -29,7 +29,16 @@ const Hero = () => {
             </div>
         </div>
         <div className={styles.photo}>
-        <img src="/assets/perfil.jpg" alt="me" />
+          <span className={styles.sphere}></span>
+          <span className={styles.sphere}></span>
+          <span><img className={styles.icons} src="./assets/icons/react-original 1.svg"  alt="React svg" style={{height: "70px", width: "70px"}}></img></span>
+          <span><img className={styles.icons} src="./assets/icons/javascript-original 1.svg" alt="Javascript svg" style={{height: "35px", width: "35px"}}></img></span>
+          <span><img className={styles.icons} src="./assets/icons/nodejs-original 1.svg" alt="Node svg" style={{height: "60px", width: "60px"}}></img></span>
+        <img src="/assets/perfil-disney.png" alt="me" style={{height: "230px", width: "230px", zIndex: "0"}}/>
+      </div>
+
+      <div className={styles.sobre}>
+      
       </div>
       </div>
       
