6da7d3d961a03e044173d124763dfaa5159a9870
João Pedro C. Carvalho <60660447+pedro-jp@users.noreply.github.com>
2024-02-06 16:51:12 +0000

    feat: add svg icons

---
+++ portfolio/src/pages/components/svg/Svg.js
@@ -0,0 +1,22 @@
+import styles from "./Svg.module.css"
+
+function Svg() {
+    return(
+        <>
+            <img src="./assets/icons/css3-original 1.svg" alt="css icon"></img>
+            <img src="./assets/icons/html5-original 1.svg" alt="html icon"></img>
+            <img src="./assets/icons/javascript-original 1.svg" alt="javascript icon"></img>
+            <img src="./assets/icons/linux-original 1.svg" alt="linux icon"></img>
+            <img src="./assets/icons/nodejs-original 1.svg" alt="node icon"></img>
+            <img src="./assets/icons/python-original 1.svg" alt="python icon"></img>
+            <img src="./assets/icons/react-original 1.svg" alt="react icon"></img>
+            <img src="./assets/icons/typescript-original 2.svg" alt="typescript icon"></img>
+            <img src="./assets/icons/nextjs-original 1.svg" alt="next icon"></img>
+            <img src="./assets/icons/docker.svg" alt="docker icon"></img>
+            <img src="./assets/icons/mysql.svg" alt="mysql icon"></img>
+
+        </>
+    )
+}
+
+export default Svg
