6879899eb52bb2ee7e6bd56612e1081c6841a42e
João Pedro C. Carvalho <joaopedroc035@gmail.com>
2024-02-06 15:48:40 -0300

    fix: background color bug

--- portfolio/src/pages/components/about/About.js
+++ portfolio/src/pages/components/about/About.js
@@ -4,22 +4,22 @@ function About() {
     return (
         <div id="about" className={styles.about}>
             <div>
-                <h1>
+                <h2>
                     sobreMim<span>( )</span>
-                </h1>
+                </h2>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.Suspendisse placerat cursus urna a consectetur. Vestibulum sit amet ex vehicula, efficitur odio non, mollis orci. Mauris pretium volutpat odio, id egestas quam porta eu. Mauris pharetra neque ac eleifend volutpat. Proin id imperdiet neque. Mauris viverra pulvinar orci at sodales. Vivamus leo augue, convallis nec lorem sit amet, porta molestie arcu. Etiam feugiat nibh eu lacinia pretium. Cras vitae tincidunt dolor, id efficitur lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc tincidunt turpis vitae risus imperdiet condimentum.
                 </p>
             </div>
 
             <div>
-                <div>
+                <div className={styles.fullstack}>
                     <div>
                         <h2>Fullstack Developer</h2><span><h2>{"</>"}</h2></span>
                     </div>
                     <a href="https://github.com/pedro-jp">Projetos</a>
                 </div>
-                <div>
+                <div className={styles.fullstack}>
                     <div>
                         <h2>Freelancer</h2><span><h2>{"{ }"}</h2></span>
                     </div>
